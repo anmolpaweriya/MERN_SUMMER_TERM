9dfb1b6a7f07bc060f4641b79b03ff267b1e7c9a
anmolpaweriya <paweriyaanmol@gmail.com>
2026-06-11 19:52:27 +0530

    problem statement2 done

--- problems.js
+++ problems.js
@@ -3,17 +3,26 @@ Problem Statement : Convert the given objects of array in below format max marks
 */
 
 const students = [
-  { name: "John", marks: [15, 20, 30, 29, 25] },
-  { name: "Jane", marks: [30, 24, 10, 19, 22] },
-  { name: "Bob", marks: [20, 21, 30, 23, 15] },
-  { name: "Alice", marks: [18, 10, 5, 3, 10] },
+  { name: "John", marks: [15, 20, 30, 29, 25], course: "Full Stack" },
+  { name: "Jane", marks: [30, 24, 10, 19, 22], course: "DSA" },
+  { name: "Bob", marks: [20, 21, 30, 23, 15], course: "Backend" },
+  { name: "Alice", marks: [18, 10, 5, 3, 10], course: "Frontend" },
 ];
 
 const result = students.map((student) => {
   const totalMarks = student.marks.reduce((acc, mark) => acc + mark, 0);
   const percentage = (totalMarks / 150) * 100;
   const status = percentage > 60 ? "pass" : "fail";
-  return { name: student.name, percentage: percentage.toFixed(2), status };
+  return {
+    name: student.name,
+    percentage: percentage.toFixed(2),
+    status,
+    course: student.course,
+  };
 });
 
 console.log(result);
+
+// use filter to get only passing students
+const passingStudents = result.filter((student) => student.status === "pass");
+console.log(passingStudents);
